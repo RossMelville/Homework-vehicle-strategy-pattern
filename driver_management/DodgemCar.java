0a28e35abcdbaf65c332fc9da1a8c0783268ad2b
Ross Melville <rossmelville@hotmail.com>
2017-08-02 16:04:14 +0100

    Drive distance method calculation added.

--- driver_management/DodgemCar.java
+++ driver_management/DodgemCar.java
@@ -1,9 +1,16 @@
 package driver_management;
 import behaviours.*;
 
-public class DodgemCar implements Drivable {
+public class DodgemCar implements Driveable {
+  private int averageSpeed;
+
+  public DodgemCar(int averageSpeed) {
+    this.averageSpeed = averageSpeed;
+  }
 
   public int driveDistance(int distance) {
-    return 100;
+    return distance/this.averageSpeed;
   }
+
+
 }
